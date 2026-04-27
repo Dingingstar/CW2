06027fe77d3125b33e6b6e139838396c1d39ab2f
ssyyd14 <ssyyd14@MET406-P350-122>
2026-04-27 13:20:24 +0800

    preliminary task

--- Script_template.m
+++ Script_template.m
@@ -1,10 +1,16 @@
-% Insert name here
-% Insert email address here
-
+% Yixin Ding
+% ssyyd14@Nottingham.edu.cn
+clear all
 
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
-
-% Insert answers here
+a = arduino('COM5', 'Uno');
+for i = 1:10
+    writeDigitalPin(a, 'D13', 1); 
+    pause(0.5);
+    
+    writeDigitalPin(a, 'D13', 0); 
+    pause(0.5); 
+end
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 
