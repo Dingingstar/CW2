2593c9a2249389f213ae8dde4092457a40828241
ssyyd14 <ssyyd14@MET406-P350-122>
2026-04-28 20:17:45 +0800

    Add pre task comments

--- Script_template.m
+++ Script_template.m
@@ -1,16 +1,16 @@
 % Yixin Ding
 % ssyyd14@Nottingham.edu.cn
-clear all
+clear all % Clear all variables from MATLAB workspace to ensure clean startup
 
 % PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
-a = arduino('COM5', 'Uno');
-% for i = 1:10
-%     writeDigitalPin(a, 'D13', 1); 
-%     pause(0.5);
-% 
-%     writeDigitalPin(a, 'D13', 0); 
-%     pause(0.5); 
-% end
+a = arduino('COM5', 'Uno'); % Establish MATLAB-Arduino communication: connect to Arduino Uno on COM5 port
+for i = 1:10 % FOR loop: Repeat LED blink 10 times
+    writeDigitalPin(a, 'D13', 1); % Set digital pin D13 to HIGH (turn LED ON)
+    pause(0.5); % Keep LED ON for 0.5 seconds
+
+    writeDigitalPin(a, 'D13', 0); % Set digital pin D13 to LOW (turn LED OFF)
+    pause(0.5); % Keep LED OFF for 0.5 seconds
+end
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 % % b)
@@ -79,9 +79,8 @@ a = arduino('COM5', 'Uno');
 
 
 %% TASK 3 - ALGORITHMS – TEMPERATURE PREDICTION [30 MARKS]
-temp_prediction(a);
+% temp_prediction(a);
 
 
 %% TASK 4 - REFLECTIVE STATEMENT [5 MARKS]
-
-% Insert answers here
+% In Word.
