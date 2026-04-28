e06f007ce2988dfd2acf705f2cfb5ce8a823fffb
ssyyd14 <ssyyd14@MET406-P350-122>
2026-04-28 22:37:36 +0800

    3

--- Yixin_Ding_20719599.m
+++ Yixin_Ding_20719599.m
@@ -90,4 +90,9 @@ fclose(fileID); % Close the text file to ensure data is saved correctly
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
 temp_monitor(a);
 
+%% TASK 3 - ALGORITHMS – TEMPERATURE PREDICTION [30 MARKS]
+temp_prediction(a);
 
+
+%% TASK 4 - REFLECTIVE STATEMENT [5 MARKS]
+% In Word.
